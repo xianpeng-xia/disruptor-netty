42b8c815f3b88ee63c821a9f29d48dda97822792
夏先鹏 <951256916>
2022-05-31 22:19:43 +0800

    common

---
+++ disruptor-netty-common/src/main/java/com/example/disruptornetty/common/DisruptorNettyCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example.disruptornetty.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisruptorNettyCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisruptorNettyCommonApplication.class, args);
+    }
+
+}
---
+++ disruptor-netty-common/src/test/java/com/example/disruptornetty/common/DisruptorNettyCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.disruptornetty.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisruptorNettyCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
